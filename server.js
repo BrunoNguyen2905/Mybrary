d52bb07c78d668ed8bd127003f247b10294dc39c
BrunoNguyen2905 <nhannguyen290520@gmail.com>
2020-08-09 14:18:16 +0300

    intial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+if(process.env.NODE_ENV !== "production"){ //check if running in the production environment
+    require('dotenv').config();
+}
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout'); //set up default layout for every pages
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}); //Mongoose provides options to work around these deprecation warnings
+const db = mongoose.connection
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('comnected to Mongoose'));
+
+app.use(indexRouter);
+
+app.listen(process.env.PORT || 3000);
